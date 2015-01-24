9162155c7f7413a5119738582b4e80d2dbc833a2
adar <adar@kon.de>
2015-01-24 13:08:49 +0100

    work at config

--- src/config/Config.php
+++ src/config/Config.php
@@ -40,6 +40,66 @@ class Config {
     }
 
     /**
+     * hasItem
+     *
+     * @param ... $params
+     * @return boolean
+     */
+    public function hasItem( ...$params ) {
+        return ($this->arrayPathValue($this->config, implode('/', $params)) !== null);
+    }
+
+    /**
+     * @param ...$params
+     *
+     * @return mixed
+     */
+    public function getItem( ...$params ) {
+
+        $config = $this->config;
+
+        $sectionOrValue = $this->arrayPathValue($config, implode('/', $params));
+
+        if ( !$sectionOrValue ) {
+            throw new ConfigException("Could not find ".implode('/', $params)." in actual config");
+        }
+
+        return $sectionOrValue;
+    }
+
+    /**
+     * Get value of an array by using "root/branch/leaf" notation
+     *
+     * shamelessly borrowed from http://codeaid.net/php/get-values-of-multi-dimensional-arrays-using-xpath-notation
+     *
+     * @param array $array   Array to traverse
+     * @param string $path   Path to a specific option to extract
+     * @return mixed
+     */
+    private function arrayPathValue(array $array, $path) {
+        // specify the delimiter
+        $delimiter = '/';
+
+        if (empty($path)) {
+            throw new ConfigException('Path cannot be empty');
+        }
+
+        $path = trim($path, $delimiter);
+        $value = $array;
+        $parts = explode($delimiter, $path);
+
+        foreach ($parts as $part) {
+            if (isset($value[$part])) {
+                $value = $value[$part];
+            } else {
+                return null;
+            }
+        }
+
+        return $value;
+    }
+
+    /**
      * @param $section
      * @param $params
      *
@@ -50,12 +110,12 @@ class Config {
 
         $sectionName = $params[ 0 ];
 
-        if ( isset( $section[ $sectionName ] ) && !$section[ $sectionName ] instanceof ConfigSection ) {
+        if ( isset( $section[ $sectionName ] ) && !is_array($section[$sectionName]) ) {
             throw new ConfigException( "Cannot overwrite " . $sectionName . "." );
         }
 
         if ( !isset( $section[ $sectionName ] ) ) {
-            $section[ $sectionName ] = new ConfigSection();
+            $section[ $sectionName ] = [];
         }
         array_shift( $params );
 
@@ -63,20 +123,10 @@ class Config {
             $this->parseParams( $section[ $sectionName ], $params );
         }
         else {
-            $section[ $sectionName ] = new ConfigSection( $sectionName, $params );
+            $section[ $sectionName ] = $params[0];
         }
 
         return $section[ $sectionName ];
-    }
-
-    /**
-     * @param ...$params
-     *
-     * @return mixed
-     */
-    public function getItem( ...$params ) {
-
-        print_r( $this->config[ $params[ 0 ] ][ $params[ 1 ] ][ $params[ 2 ] ] );
     }
 }
 
